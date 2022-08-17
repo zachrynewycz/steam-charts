06f3a5ddacbe18de0010ed9e68b1258b6560d944
Zach <hrynewyczz@gmail.com>
2022-08-16 21:00:18 -0500

    server changes :(

--- src/server.js
+++ src/server.js
@@ -90,7 +90,7 @@ const fetchAPI = async (URL) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, './build')));
     app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, './build/index.html'));
+        res.sendFile(path.resolve(__dirname, "./build", "index.html"));
     })
 }
 
